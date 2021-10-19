c2a1d8dd1e918d63c6e3381a8469152fa71f1f38
danielf-souza <dfsouza5@gmail.com>
2021-10-19 13:50:54 +0200

    Initial commit on main branch

---
+++ content/home/teaching.md
@@ -0,0 +1,36 @@
++++
+# Custom widget.
+# An example of using the custom widget to create your own homepage section.
+# To create more sections, duplicate this file and edit the values below as desired.
+widget = "custom"
+active = true
+date = 2020-08-09
+
+# Note: a full width section format can be enabled by commenting out the `title` and `subtitle` with a `#`.
+title = "Teaching"
+#subtitle = "Average instructor rating: 4.8/5"
+
+# Order that this section will appear in.
+weight = 120
+
++++
+
+### ECO0008: Macroeconomics
+Teaching Assistant (TA): ESOMAS Department, University of Turin.
+Period: 2019-present
+
+### MAN0158: Political Economy
+Teaching Assistant (TA): Department of Management, University of Turin.
+Period: 2020-present
+
+### ECO0001: Microeconomics
+Teaching Assistant (TA): Department of Management, University of Turin.
+Period: 2019-2020
+
+### ECO0007: Macroeconomics
+Teaching Assistant (TA): Department of Management, University of Turin.
+Period: 2019-2020
+
+### Econometrics
+Teaching Assistant (TA): Departament of Economics. Federal University of Paraná.
+Period: 2016
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# This script allows you to easily and quickly generate and deploy your website
+# using Hugo to your personal GitHub Pages repository. This script requires a
+# certain configuration, run the `setup.sh` script to configure this. See
+# https://hjdskes.github.io/blog/deploying-hugo-on-personal-github-pages/index.html
+# for more information.
+# Set the English locale for the `date` command.
+export LC_TIME=en_US.UTF-8
+# GitHub username.
+USERNAME=danielf-souza
+# Name of the branch containing the Hugo source files.
+SOURCE=hugo
+# The commit message.
+MESSAGE="Site rebuild $(date)"
+msg() {
+    printf "\033[1;32m :: %s\n\033[0m" "$1"
+}
+msg "Pulling down the \`main\` branch into \`public\` to help avoid merge conflicts"
+git subtree pull --prefix=public \
+    git@github.com:danielf-souza/danielf-souza.github.io.git origin main -m "Merge origin main"
+msg "Building the website"
+hugo
+msg "Pushing the updated \`public\` folder to the \`sources\` branch"
+git add public
+git commit -m "$MESSAGE"
+git push origin "sources"
+msg "Pushing the updated \`public\` folder to the \`main\` branch"
+git subtree push --prefix=public \
+    git@github.com:danielf-souza/danielf-souza.github.io.git main
